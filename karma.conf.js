03681b788c65b03737fe9b514fb061d15c511157
Hugo <hugoduar1@gmail.com>
2023-02-09 04:24:37 -0600

    Delete files

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/carouselNg12App'),
+      dir: require('path').join(__dirname, '../../coverage/angular-responsive-carousel'),
       subdir: '.',
       reporters: [
         { type: 'html' },
